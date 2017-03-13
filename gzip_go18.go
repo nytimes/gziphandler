21ae80e5cb0bebb43b7e8d4a982298285a2cb953
Romain Menke <romainmenke@gmail.com>
2017-03-14 00:03:41 +0100

    shorten Push description
    shorten Push description

--- gzip_go18.go
+++ gzip_go18.go
@@ -4,24 +4,7 @@ package gziphandler
 
 import "net/http"
 
-// Push initiates an HTTP/2 server push. This constructs a synthetic
-// request using the given target and options, serializes that request
-// into a PUSH_PROMISE frame, then dispatches that request using the
-// server's request handler. If opts is nil, default options are used.
-//
-// The target must either be an absolute path (like "/path") or an absolute
-// URL that contains a valid host and the same scheme as the parent request.
-// If the target is a path, it will inherit the scheme and host of the
-// parent request.
-//
-// The HTTP/2 spec disallows recursive pushes and cross-authority pushes.
-// Push may or may not detect these invalid pushes; however, invalid
-// pushes will be detected and canceled by conforming clients.
-//
-// Handlers that wish to push URL X should call Push before sending any
-// data that may trigger a request for URL X. This avoids a race where the
-// client issues requests for X before receiving the PUSH_PROMISE for X.
-//
+// Push initiates an HTTP/2 server push.
 // Push returns ErrNotSupported if the client has disabled push or if push
 // is not supported on the underlying connection.
 func (w *GzipResponseWriter) Push(target string, opts *http.PushOptions) error {
