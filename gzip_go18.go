103757d327f428b9523e29cf7d75a3f1e5467e51
Romain Menke <romainmenke@gmail.com>
2017-03-19 13:31:15 +0100

    H2 Push : add Accept-Encoding header if missing & tests for adding headers

--- gzip_go18.go
+++ gzip_go18.go
@@ -10,7 +10,34 @@ import "net/http"
 func (w *GzipResponseWriter) Push(target string, opts *http.PushOptions) error {
 	pusher, ok := w.ResponseWriter.(http.Pusher)
 	if ok && pusher != nil {
-		return pusher.Push(target, opts)
+		return pusher.Push(target, setAcceptEncodingForPushOptions(opts))
 	}
 	return http.ErrNotSupported
+}
+
+// setAcceptEncodingForPushOptions sets "Accept-Encoding" : "gzip" for PushOptions without overriding existing headers.
+func setAcceptEncodingForPushOptions(opts *http.PushOptions) *http.PushOptions {
+
+	if opts == nil {
+		opts = &http.PushOptions{
+			Header: http.Header{
+				acceptEncoding: []string{"gzip"},
+			},
+		}
+		return opts
+	}
+
+	if opts.Header == nil {
+		opts.Header = http.Header{
+			acceptEncoding: []string{"gzip"},
+		}
+		return opts
+	}
+
+	if encoding := opts.Header.Get(acceptEncoding); encoding == "" {
+		opts.Header.Add(acceptEncoding, "gzip")
+		return opts
+	}
+
+	return opts
 }
